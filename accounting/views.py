7c188066dc02e18d3f974b93369937cf38fda8b7
tothgabor <tothgabor2003@gmail.com>
2024-08-31 13:41:49 +0200

    added styles and main page start

--- accounting/views.py
+++ accounting/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
-
-# Create your views here.
-
+import django.template.loader as loader
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    template = loader.get_template('accounting/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+    
+def home(request):
+    return HttpResponse('Home page')
+
+def about(request):
+    return HttpResponse('About page')
+
+def contact(request):    
+    return HttpResponse('Contact page')
