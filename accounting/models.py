c0a2d65299537dcb2ae96103ac3b6dfe74d91654
tothgabor <tothgabor2003@gmail.com>
2024-08-31 13:51:49 +0200

    new db schema

--- accounting/models.py
+++ accounting/models.py
@@ -6,7 +6,6 @@ class Tenant(models.Model):
     tenant_name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     phone_number = models.CharField(max_length=20)
-    square_feet = models.IntegerField()
 
     def __str__(self):
         return f'{self.unit_number} - {self.tenant_name}'
@@ -17,7 +16,7 @@ class Charge(models.Model):
     description = models.CharField(max_length=255)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     due_date = models.DateField()
-    unit = models.ForeignKey(Tenant, on_delete=models.CASCADE, related_name='charges')
+    tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE, related_name='charges')
 
     def __str__(self):
         return f'Charge {self.description} for {self.unit.unit_number}'
@@ -27,7 +26,6 @@ class Payment(models.Model):
     payment_id = models.AutoField(primary_key=True)
     amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
     payment_date = models.DateField()
-    unit = models.ForeignKey(Tenant, on_delete=models.CASCADE, related_name='payments')
     charge = models.ForeignKey(Charge, on_delete=models.CASCADE, related_name='payments')
 
     def __str__(self):
